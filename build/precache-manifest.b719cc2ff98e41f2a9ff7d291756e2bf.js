ccdb927e7956f5263e12a423728fc6a92362e384
unknown <akashbadole@rediffmail.com>
2020-10-04 11:42:56 +0530

    Production build and screnshot

---
+++ build/precache-manifest.b719cc2ff98e41f2a9ff7d291756e2bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b8d40cb58f7b2da4e203531c4a88a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969edef66bc02b50e9d1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7d4d0c9a48dbf7aa55cd",
+    "url": "/static/css/main.9e1212f6.chunk.css"
+  },
+  {
+    "revision": "969edef66bc02b50e9d1",
+    "url": "/static/js/2.854ee6ce.chunk.js"
+  },
+  {
+    "revision": "ea859dfb7399c11544e011121961d542",
+    "url": "/static/js/2.854ee6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4d0c9a48dbf7aa55cd",
+    "url": "/static/js/main.c2a460c0.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
